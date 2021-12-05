21661a42ad69042e9759463de05b9f756fd4868f
jgustafsson <johan.gustafsson@recordedfuture.com>
2021-12-05 10:51:25 -0800

    Day 1: Sonar Sweep

---
+++ day01/SonarSweep.scala
@@ -0,0 +1,17 @@
+import scala.io.Source
+
+object SonarSweep {
+  def solution(list: List[Int], window: Int) = {
+    val result: Int = list.sliding(window).count(pair => {
+      pair(window - 1) > pair(0)
+    })
+    println(result)
+  }
+
+  def main(args: Array[String]): Unit = {
+    val source = Source.fromFile("input2.txt")
+    val list: List[Int] = source.getLines().map(_.toInt).toList
+    solution(list, 2) // 1692
+    solution(list, 4) // 1724
+  }
+}
